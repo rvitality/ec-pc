66c26333d6928fcacd2d436f76a0440e191f0d1f
rvitality <vital.renzjohn@gmail.com>
2022-11-03 22:24:48 +0800

    Add fetch sarima rate when App component mounts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Calculator from "./components/Calculator/Calculator.component";
 import Header from "./components/Header/Header.component";
 import Graph from "./components/Graph/Graph.component";
 
 import "./App.styles.scss";
 import Modal from "./components/Modal/Modal.component";
+import OfficialRateForm from "./components/OfficialRateForm/OfficialRateForm.component";
+
+import { useApplianceContext } from "./context/ApplianceContext";
 
 const App = () => {
+    const { setSarimaRate } = useApplianceContext();
+
+    // fetch sarima rate
+    useEffect(() => {
+        const sendRequest = async () => {
+            const response = await fetch("/sarima_rate");
+            if (!response.ok) return "Something went wrong!";
+            const data = await response.json();
+            console.log(data);
+            const { accuracy, sarima_rate } = data;
+            setSarimaRate(sarima_rate);
+        };
+        sendRequest();
+    }, []);
+
     return (
         <main>
             <Header />
             <section className="main-content">
                 <Calculator />
-                <Graph />
+                <div className="right-side">
+                    <Graph />
+                    {/* <OfficialRateForm /> */}
+                </div>
             </section>
             <Modal />
         </main>
