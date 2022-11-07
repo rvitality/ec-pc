903a417fc03ff4a09f3cb72013ea1785a23eb038
rvitality <vital.renzjohn@gmail.com>
2022-11-07 23:07:03 +0800

    Add routes to requests

--- backend/server.py
+++ backend/server.py
@@ -1,21 +1,39 @@
-from flask import Flask
+from flask import Flask, request
+import csv
 
-from sarima_prediction import *
+from sarima_prediction import predict_rate
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
-    return "Hello, World!"
+@app.route("/get_sarima_rate")
+def get_sarima_rate():
+    forecast = predict_rate()
+    predicted_rate = forecast.values[0]
+    return {
+        "sarima_rate": predicted_rate,
+    }
 
 
-@app.route("/sarima_rate")
-def prediction():
-    sarima_rate = forecast.tolist()[0]
-    accuracy = rmse
-    return {"sarima_rate": sarima_rate, "accuracy": accuracy}
+@app.route("/push_official_rate", methods=["GET", "POST"])
+def push_official_rate():
+    if request.method == "POST":
+        jsonData = request.get_json()
+        entered_data = list(jsonData.values())
+
+        with open("official_rates.csv", "a", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow(entered_data)
+
+        # run prediction
+        next_month_predicted_rate = get_sarima_rate()
+
+        return next_month_predicted_rate
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+# with open("official_rates.csv", "r") as rates:
+#     print(rates.readlines())
