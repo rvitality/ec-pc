cbddad67e7566523e8276fbfa8c9ea11df6dd36c
rvitality <vital.renzjohn@gmail.com>
2022-11-07 23:03:09 +0800

    Wrap App component with AuthContextProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import { AuthContextProvider } from "./context/AuthContext";
 import { ApplianceContextProvider } from "./context/ApplianceContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <ApplianceContextProvider>
-            <App />
-        </ApplianceContextProvider>
+        <AuthContextProvider>
+            <ApplianceContextProvider>
+                <App />
+            </ApplianceContextProvider>
+        </AuthContextProvider>
     </React.StrictMode>
 );
