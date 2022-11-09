a02ef9cfb169e745c76fe0b1b2ac75b6bdda98e4
rvitality <vital.renzjohn@gmail.com>
2022-11-09 20:40:58 +0800

    Add broswer router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
 import "./index.css";
 import App from "./App";
 
@@ -9,10 +11,12 @@ import { ApplianceContextProvider } from "./context/ApplianceContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <AuthContextProvider>
-            <ApplianceContextProvider>
-                <App />
-            </ApplianceContextProvider>
-        </AuthContextProvider>
+        <BrowserRouter>
+            <AuthContextProvider>
+                <ApplianceContextProvider>
+                    <App />
+                </ApplianceContextProvider>
+            </AuthContextProvider>
+        </BrowserRouter>
     </React.StrictMode>
 );
