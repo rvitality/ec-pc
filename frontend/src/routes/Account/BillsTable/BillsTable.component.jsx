4802af2bc6c28057600f89d36881ab55bfe38898
rvitality <vital.renzjohn@gmail.com>
2022-11-11 20:30:59 +0800

    Add a component that includes a table for the user's bills

---
+++ frontend/src/routes/Account/BillsTable/BillsTable.component.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import "./BillsTable.styles.scss";
+
+const BillsTable = ({ dataToDisplay }) => {
+    return (
+        <table className="data-bills">
+            <thead>
+                <tr>
+                    <th>No.</th>
+                    <th>Month</th>
+                    <th>Year</th>
+                    <th>Forecasted Bills</th>
+                    <th>Actual Bills</th>
+                    <th>Accuracy</th>
+                </tr>
+            </thead>
+            <tbody>
+                {dataToDisplay.map((log, index) => {
+                    const { id, month, year, forecasted, actual, accuracy, status } = log;
+
+                    return (
+                        <tr key={id}>
+                            <td>{index + 1}</td>
+                            <td>{month}</td>
+                            <td>{year}</td>
+                            <td>₱ {forecasted.toLocaleString()}</td>
+                            <td>₱ {actual.toLocaleString()}</td>
+                            <td className={`status ${status}`}>{accuracy}%</td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
+};
+
+export default BillsTable;
