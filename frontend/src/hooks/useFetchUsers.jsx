9b1139c16ecd89f5d6ba587606888f0ac27b4544
rvitality <vital.renzjohn@gmail.com>
2022-11-16 22:10:29 +0800

    Add custom hook to fetch users

---
+++ frontend/src/hooks/useFetchUsers.jsx
@@ -0,0 +1,35 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { getUsers } from "../utils/firebase.utils";
+
+const useFetchUsers = () => {
+    const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [users, setUsers] = useState([]);
+
+    // ! FETCH USER BILLS FROM FIRESTORE DB
+    useEffect(() => {
+        console.log(123);
+
+        setIsLoading(true);
+        const fetchBills = async () => {
+            try {
+                const response = await getUsers();
+                if (response) {
+                    setUsers(response);
+                }
+                // setUsers
+            } catch (err) {
+                setError(err.message);
+                console.log(err.message);
+            }
+
+            setIsLoading(false);
+        };
+        fetchBills();
+    }, []);
+
+    return { error, isLoading, data: users };
+};
+
+export default useFetchUsers;
