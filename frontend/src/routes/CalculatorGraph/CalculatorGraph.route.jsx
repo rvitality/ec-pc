0d0b61b79e3387e9754e899ab07b324749d472a0
rvitality <vital.renzjohn@gmail.com>
2022-11-16 21:28:21 +0800

    Add sarima rate fetch request

--- frontend/src/routes/CalculatorGraph/CalculatorGraph.route.jsx
+++ frontend/src/routes/CalculatorGraph/CalculatorGraph.route.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-import "./CalculatorGraph.styles.scss";
+import { useApplianceContext } from "../../context/ApplianceContext";
 
 import Calculator from "../../components/Calculator/Calculator.component";
 import Modal from "../../components/Modal/Modal.component";
 import Graph from "../../components/Graph/Graph.component";
 
+import "./CalculatorGraph.styles.scss";
+
 const CalculatorGraph = () => {
+    const { setSarimaRate } = useApplianceContext();
+
+    // ! fetch sarima rate
+    useEffect(() => {
+        const sendRequest = async () => {
+            try {
+                const response = await fetch("/get_sarima_rate");
+                if (!response.ok) return "Something went wrong!";
+                const data = await response.json();
+                console.log(data);
+                const { sarima_rate } = data;
+                setSarimaRate(sarima_rate);
+            } catch (err) {
+                console.log(err.message);
+            }
+        };
+        sendRequest();
+    }, []);
+
     return (
         <>
             <section className="main-content">
