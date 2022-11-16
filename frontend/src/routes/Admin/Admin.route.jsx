c0cb14399b8439b18465a8e2f58a923d316e0619
rvitality <vital.renzjohn@gmail.com>
2022-11-16 22:11:34 +0800

    Add admin route with users and official rate

--- frontend/src/routes/Admin/Admin.route.jsx
+++ frontend/src/routes/Admin/Admin.route.jsx
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Users from "./Users/Users.component";
+import OfficialRateForm from "./OfficialRateForm/OfficialRateForm.component";
+
+import useFetchUsers from "../../hooks/useFetchUsers";
 
 import "./Admin.styles.scss";
 
 const Admin = () => {
-    return <div>Admin</div>;
+    const usersRequestResponse = useFetchUsers();
+
+    const [selectedCategory, setSelectedCategory] = useState("users");
+
+    const clickCategoryHandler = category => {
+        setSelectedCategory(category);
+    };
+
+    let contentToShow;
+
+    if (selectedCategory === "rate-form") {
+        contentToShow = <OfficialRateForm />;
+    } else if (selectedCategory === "users") {
+        contentToShow = <Users response={usersRequestResponse} />;
+    }
+
+    return (
+        <section className="admin-dashboard section-px section-py">
+            <h1 className="heading-secondary">Admin Dashboard</h1>
+
+            <nav>
+                <ul>
+                    <li
+                        className={selectedCategory === "users" ? "active" : ""}
+                        onClick={() => clickCategoryHandler("users")}
+                    >
+                        Users
+                    </li>
+                    <li
+                        className={selectedCategory === "rate-form" ? "active" : ""}
+                        onClick={() => clickCategoryHandler("rate-form")}
+                    >
+                        Offical Rate
+                    </li>
+                </ul>
+            </nav>
+
+            <div className="content">{contentToShow}</div>
+        </section>
+    );
 };
 
 export default Admin;
