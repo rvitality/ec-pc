b050363feb8f2c7d8bd0791d347fed92c3dfd67c
rvitality <vital.renzjohn@gmail.com>
2022-11-16 22:13:59 +0800

    Add table for different types of users

---
+++ frontend/src/routes/Admin/UsersTable/UsersTable.component.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import "./UsersTable.styles.scss";
+
+const UsersTable = ({ dataToDisplay }) => {
+    return (
+        <table className="table users-table data-logs">
+            <thead>
+                <tr>
+                    <th>Display Picture</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                </tr>
+            </thead>
+            <tbody>
+                {dataToDisplay.map((user, index) => {
+                    const { id, name, email, role, photoURL } = user;
+
+                    return (
+                        <tr key={id}>
+                            <td className="photo">
+                                <img src={photoURL} alt={name} />
+                            </td>
+                            <td>
+                                <strong>{name}</strong>{" "}
+                            </td>
+                            <td>{email}</td>
+                            <td className="role">{role}</td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
+};
+
+export default UsersTable;
