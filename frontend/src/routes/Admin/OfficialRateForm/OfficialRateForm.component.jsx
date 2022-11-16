6abdedfa93ac430cb51ddd31de903f45d64f0af3
rvitality <vital.renzjohn@gmail.com>
2022-11-16 22:12:50 +0800

    Move official rate form component in another directory

--- frontend/src/routes/Admin/OfficialRateForm/OfficialRateForm.component.jsx
+++ frontend/src/routes/Admin/OfficialRateForm/OfficialRateForm.component.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import { useApplianceContext } from "../../context/ApplianceContext";
+import { useApplianceContext } from "../../../context/ApplianceContext";
 
 import "./OfficialRateForm.styles.scss";
 
