0051fc037f86234d64d55b096b669fece5ea6563
rvitality <vital.renzjohn@gmail.com>
2022-11-18 08:58:16 +0800

    Fetch last row of data from official rates csv on component mount

--- frontend/src/routes/Admin/OfficialRateForm/OfficialRateForm.component.jsx
+++ frontend/src/routes/Admin/OfficialRateForm/OfficialRateForm.component.jsx
@@ -1,12 +1,37 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { useApplianceContext } from "../../../context/ApplianceContext";
 
 import "./OfficialRateForm.styles.scss";
 
 const OfficialRateForm = () => {
     const { setSarimaRate } = useApplianceContext();
-
     const rateInputRef = useRef();
+
+    const [currentOfficialRate, setCurrentOfficialRate] = useState([]);
+    const date = (currentOfficialRate && currentOfficialRate[0]) || 0;
+    const rate = (currentOfficialRate && +currentOfficialRate[1]) || 0;
+
+    // ! fetch last rate from csv file official rates
+    useEffect(() => {
+        const sendRequest = async () => {
+            try {
+                const response = await fetch("/get_last_rate_data");
+                if (!response.ok) return "Something went wrong!";
+                const data = await response.json();
+                const { last_rate_data } = data;
+                setCurrentOfficialRate(last_rate_data);
+
+                // const rateInput = rateInputRef.current?.value;
+                // setCurrentOfficialRate(prevState => {
+                //     last_rate_data[-1] = rateInput;
+                //     return [...last_rate_data];
+                // });
+            } catch (err) {
+                console.log(err.message);
+            }
+        };
+        sendRequest();
+    }, []);
 
     const currentDate = new Date();
     const monthLong = currentDate.toLocaleDateString("en-US", { month: "long" });
@@ -17,6 +42,11 @@ const OfficialRateForm = () => {
         e.preventDefault();
 
         const rateInput = rateInputRef.current?.value;
+
+        setCurrentOfficialRate(prevState => {
+            prevState[prevState.length - 1] = rateInput;
+            return [...prevState];
+        });
 
         const month = currentDate.getMonth() + 1;
         const date = `${yr}/${month}/1`;
@@ -34,13 +64,12 @@ const OfficialRateForm = () => {
                     }),
                 });
 
-                console.log(response);
-
                 if (!response.ok) throw Error("Something went wrong");
 
                 const data = await response.json();
-                console.log(data);
                 const { sarima_rate } = data;
+
+                console.log("Official Rate Form: ", data);
                 setSarimaRate(sarima_rate);
             } catch (err) {
                 console.log(err);
@@ -55,8 +84,16 @@ const OfficialRateForm = () => {
             <div className="date">
                 <span className="label">Date Today: </span> {monthLong} {day}, {yr}
             </div>
+            <div className="official-rate-form__control rate">
+                <label>
+                    Current Rate <small>({date}): </small>
+                </label>
+                <div className="sarima-rate">₱ {rate.toFixed(2)}</div>
+            </div>
             <div className="official-rate-form__control">
-                <label>Official Rate: </label>
+                <label>
+                    <strong>{123 !== 1 ? "Update" : "Enter"}</strong> Rate:{" "}
+                </label>
                 <input type="number" step="any" ref={rateInputRef} required />
             </div>
             <button type="submit" className="btn primary">
