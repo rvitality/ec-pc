f085a76376fef094ac3bf2d63098ed5743436348
rvitality <vital.renzjohn@gmail.com>
2022-11-22 19:51:07 +0800

    Add loading spinner and use a custom hook instead of props

--- frontend/src/routes/Admin/Users/Users.component.jsx
+++ frontend/src/routes/Admin/Users/Users.component.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 
+import useFetchUsers from "../../../hooks/useFetchUsers";
+
+import Spinner from "../../../ui/Spinner/Spinner.ui";
 import DataTable from "../../../components/DataTable/DataTable.component";
-import UsersTable from "../UsersTable/UsersTable.component";
+import UsersTable from "./UsersTable/UsersTable.component";
 import "./Users.styles.scss";
 
-const Users = props => {
-    const { error, isLoading, data: users } = props.response;
+const Users = () => {
+    const { error, isLoading, data: users } = useFetchUsers();
 
     return (
         <div className="users-container">
             {!error && isLoading ? (
-                <h1>Loading...</h1>
+                <div className="loading-container">
+                    <div className="loading-container__text">Loading...</div>
+                    <Spinner />
+                </div>
             ) : (
                 <DataTable data={users} Table={UsersTable} />
             )}
