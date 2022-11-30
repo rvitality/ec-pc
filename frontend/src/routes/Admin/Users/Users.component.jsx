9098bde5e1990bfb55ef0438452326f94848ed41
rvitality <vital.renzjohn@gmail.com>
2022-11-30 22:25:49 +0800

    Add table search functionality

--- frontend/src/routes/Admin/Users/Users.component.jsx
+++ frontend/src/routes/Admin/Users/Users.component.jsx
@@ -8,6 +8,19 @@ import UsersTable from "./UsersTable/UsersTable.component";
 import "./Users.styles.scss";
 
 const Users = () => {
+    const filterBySearchHandler = (data, inputValue) => {
+        if (!data) return;
+
+        return data.filter(user => {
+            const { email, name, role } = user;
+            return (
+                email.toLowerCase().includes(inputValue) ||
+                name.toLowerCase().includes(inputValue) ||
+                role.toLowerCase().includes(inputValue)
+            );
+        });
+    };
+
     const reqCollectionsResponse = useFetchCollections("users");
     const { error, isLoading, collections: users } = reqCollectionsResponse;
 
@@ -19,7 +32,11 @@ const Users = () => {
                     <Spinner />
                 </div>
             ) : (
-                <DataTable data={users || []} Table={UsersTable} />
+                <DataTable
+                    data={users || []}
+                    Table={UsersTable}
+                    onFilterBySearch={filterBySearchHandler}
+                />
             )}
         </div>
     );
