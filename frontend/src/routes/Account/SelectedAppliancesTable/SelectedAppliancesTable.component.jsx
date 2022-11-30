040dd8a9fcad0f0345f95c7db30a7a96f142a055
rvitality <vital.renzjohn@gmail.com>
2022-11-30 22:18:43 +0800

    Add delete appliance feature

--- frontend/src/routes/Account/SelectedAppliancesTable/SelectedAppliancesTable.component.jsx
+++ frontend/src/routes/Account/SelectedAppliancesTable/SelectedAppliancesTable.component.jsx
@@ -1,10 +1,46 @@
 import React from "react";
 
+import { TiEdit } from "react-icons/ti";
+import { MdDelete } from "react-icons/md";
+
+import { useAuthContext } from "../../../context/AuthContext";
+import { updateUserAppliances } from "../../../utils/firebase.utils";
+
 import "./SelectedAppliancesTable.styles.scss";
 
 const SelectedAppliancesTable = ({ dataToDisplay }) => {
+    const { user, setUserAppliances, setUserRecords } = useAuthContext();
+
+    // const updateApplianceHandler = appliance => {
+    //     console.log(appliance);
+    // };
+
+    const deleteApplianceHandler = appliance => {
+        const modifiedAppliances = user.selectedAppliances?.filter(
+            currentAppliance => currentAppliance.applianceID !== appliance.applianceID
+        );
+
+        setUserAppliances(modifiedAppliances);
+
+        updateUserAppliances({ id: user.id, selectedAppliances: modifiedAppliances });
+
+        const forecastedBill = modifiedAppliances.reduce(
+            (sum, item) => sum + item.applianceBill,
+            0
+        );
+
+        if (!user.records || !modifiedAppliances.length) return;
+
+        if (user.records.length > 0) {
+            const newRecords = [...user.records];
+            const lastRecord = newRecords[newRecords.length - 1];
+            lastRecord.forecasted = forecastedBill;
+            setUserRecords(newRecords);
+        }
+    };
+
     return (
-        <table className="data-table-bills table">
+        <table className="selected-appliances table">
             <thead>
                 <tr>
                     <th>No.</th>
@@ -13,6 +49,7 @@ const SelectedAppliancesTable = ({ dataToDisplay }) => {
                     <th>Quantity</th>
                     <th>Durations (hr)</th>
                     <th>Cost</th>
+                    <th>Action</th>
                 </tr>
             </thead>
             <tbody>
@@ -38,6 +75,25 @@ const SelectedAppliancesTable = ({ dataToDisplay }) => {
                                     : inputDurations.join(", ")}
                             </td>
                             <td>₱ {applianceBill.toLocaleString()}</td>
+                            <td>
+                                <div className="actions">
+                                    {/* <a
+                                        href="#new-product"
+                                        onClick={updateApplianceHandler.bind(null, log)}
+                                        title="Edit"
+                                        className="actions__edit"
+                                    >
+                                        <TiEdit />
+                                    </a> */}
+                                    <button
+                                        title="Delete"
+                                        onClick={() => deleteApplianceHandler(log)}
+                                        className="actions__del"
+                                    >
+                                        <MdDelete />
+                                    </button>
+                                </div>
+                            </td>
                         </tr>
                     );
                 })}
