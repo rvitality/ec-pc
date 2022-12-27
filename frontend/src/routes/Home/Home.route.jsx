f6a70c895cc77172b0c103f6621814e3c54f3292
rvitality <vital.renzjohn@gmail.com>
2022-12-27 21:15:30 +0800

    Remove about section

--- frontend/src/routes/Home/Home.route.jsx
+++ frontend/src/routes/Home/Home.route.jsx
@@ -32,49 +32,6 @@ const Home = () => {
                     </div>
                 </div>
             </section>
-            <section id="about" className="about section-px section-py">
-                <div className="heading">
-                    <div className="heading-secondary">About</div>
-                    <div className="section-paragraph">
-                        This is an application developed by student researchers to predict a{" "}
-                        <strong>household's electric bill for the month</strong>. However, this
-                        application is only limited to the households within{" "}
-                        <strong>Angeles City, Pampanga, Philippines</strong>. If you are from
-                        another city, place, or country, then the accuracy of this application may
-                        differ as the rate used to create this calculator is based on the public
-                        information posted by <strong>Angeles Electric Corporation</strong>.
-                    </div>
-                </div>
-
-                <h2 className="heading-tertiary our-team-heading">Our Team</h2>
-
-                <div className="people">
-                    <div className="people__item">
-                        <div>
-                            <img src={jyumulImgSrc} alt="Joshua Yumul" />
-                        </div>
-                        <h3 className="people__name">Joshua B. Yumul</h3>
-                    </div>
-
-                    <div className="people__item">
-                        <div>
-                            <img src={csorianoImgSrc} alt="Carlos Gabriel U. Soriano" />
-                        </div>
-                        <h3 className="people__name">Carlos Gabriel U. Soriano</h3>
-                    </div>
-
-                    <div className="people__item">
-                        <div>
-                            <img
-                                src={dvelascoImgSrc}
-                                alt="Danielle G. Velasco
-"
-                            />
-                        </div>
-                        <h3 className="people__name">Danielle G. Velasco</h3>
-                    </div>
-                </div>
-            </section>
         </>
     );
 };
